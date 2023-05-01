a2d768fd0c93a13d0dfd170f971692457b582fdf
cankurtur <cankurtur@gmail.com>
2023-05-01 18:09:33 +0300

    init project

---
+++ LeoCalculator/LeoCalculator/Module/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LeoCalculator
+//
+//  Created by Can Kurtur on 1.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
