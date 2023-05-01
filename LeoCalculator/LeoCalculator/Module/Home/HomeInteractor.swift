42d777c41763dee76ec8194b1f084aeed33ac11a
Can <51170501+cankurtur@users.noreply.github.com>
2023-05-01 19:50:51 +0300

    feature/home-module (#3)
    * create home module
    
    * create storyboard case and protocol

---
+++ LeoCalculator/LeoCalculator/Module/Home/HomeInteractor.swift
@@ -0,0 +1,8 @@
+//
+//  HomeInteractor.swift
+//  LeoCalculator
+//
+//  Created by Can Kurtur on 1.05.2023.
+//
+
+import Foundation
---
+++ LeoCalculator/LeoCalculator/Storyboards/StoryboardNames.swift
@@ -0,0 +1,12 @@
+//
+//  StoryboardNames.swift
+//  LeoCalculator
+//
+//  Created by Can Kurtur on 1.05.2023.
+//
+
+import Foundation
+
+public enum StoryboardNames: String {
+    case home = "Home"
+}
