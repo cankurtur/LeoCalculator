59980aac14e9aa85f6999d4929657822f35aa444
cankurtur <cankurtur@gmail.com>
2023-05-01 19:49:59 +0300

    create home module

---
+++ LeoCalculator/LeoCalculator/Module/Home/HomePresenter.swift
@@ -0,0 +1,8 @@
+//
+//  HomePresenter.swift
+//  LeoCalculator
+//
+//  Created by Can Kurtur on 1.05.2023.
+//
+
+import Foundation
---
+++ LeoCalculator/LeoCalculator/Module/Home/HomeRouter.swift
@@ -0,0 +1,8 @@
+//
+//  HomeRouter.swift
+//  LeoCalculator
+//
+//  Created by Can Kurtur on 1.05.2023.
+//
+
+import Foundation
