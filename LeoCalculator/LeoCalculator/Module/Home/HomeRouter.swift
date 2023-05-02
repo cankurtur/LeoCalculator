f7f9e5e30b8c037675cebe87d3ce5ffcac325797
cankurtur <cankurtur@gmail.com>
2023-05-03 00:27:31 +0300

    create home module

--- LeoCalculator/LeoCalculator/Module/Home/HomeRouter.swift
+++ LeoCalculator/LeoCalculator/Module/Home/HomeRouter.swift
@@ -5,4 +5,38 @@
 //  Created by Can Kurtur on 1.05.2023.
 //
 
-import Foundation
+import UIKit
+
+// MARK: - RouterInterface
+
+protocol HomeRouterInterface: RouterInterface { }
+
+// MARK: - HomeRouter
+
+final class HomeRouter {
+    private weak var presenter: HomePresenter?
+    private weak var navigationController: UINavigationController?
+    
+    init(navigationController: UINavigationController?) {
+        self.navigationController = navigationController
+    }
+    
+    static func createModule() -> UINavigationController {
+
+        let interactor = HomeInteractor()
+        let view = HomeViewController.instantiate()
+        let navCon = UINavigationController(rootViewController: view)
+        navCon.modalPresentationStyle = .fullScreen
+        let router = HomeRouter(navigationController: navCon)
+        let presenter = HomePresenter(router: router, interactor: interactor, view: view)
+        router.presenter = presenter
+        view.presenter = presenter
+        interactor.output = presenter
+        
+        return navCon
+    }
+}
+
+// MARK: - HomeRouterInterface
+
+extension HomeRouter: HomeRouterInterface { }
--- LeoCalculator/LeoCalculator/Module/Home/HomeViewController.swift
+++ LeoCalculator/LeoCalculator/Module/Home/HomeViewController.swift
@@ -7,23 +7,129 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+// MARK: - Constant
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+private enum Constant {
+    static let backgroundColor: UIColor = .clear
+    
+    enum ResultLabel {
+        static let font: UIFont = .bold50
+        static let textColor: UIColor = .white
+        static let initialText: String = "0"
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    enum NumpadButtons {
+        static let titleFont: UIFont = .bold20
+        static let titleColor: UIColor = .white
+        static let backgroundColor: UIColor = .gray
+        static let cornerRadius: CGFloat = 8
     }
-    */
+    
+    enum BasicOperationButtons {
+        static let titleFont: UIFont = .bold20
+        static let titleColor: UIColor = .black
+        static let backgroundColor: UIColor = .systemOrange
+        static let cornerRadius: CGFloat = 8
+    }
+    
+    enum ExtraOperationButtons {
+        static let titleFont: UIFont = .bold20
+        static let titleColor: UIColor = .systemOrange
+        static let backgroundColor: UIColor = .darkGray
+        static let cornerRadius: CGFloat = 8
+    }
+}
 
+// MARK: - ViewInterface
+
+protocol HomeViewInterface: ViewInterface {
+    func prepareUI()
+}
+
+// MARK: - HomeViewController
+
+final class HomeViewController: UIViewController, Storyboarded {
+    
+    @IBOutlet private weak var resultLabel: UILabel!
+    @IBOutlet private var numpadButtons: [UIButton]!
+    @IBOutlet private var basicOperationButtons: [UIButton]!
+    @IBOutlet private var extraOperationButtons: [UIButton]!
+    
+    static var storyboardName: StoryboardNames {
+        return .home
+    }
+    
+    var presenter: HomePresenterInterface!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        presenter.viewDidLoad()
+    }
+}
+
+// MARK: - HomeViewInterface
+
+extension HomeViewController: HomeViewInterface {
+    func prepareUI() {
+        view.backgroundColor = Constant.backgroundColor
+        navigationController?.setNavigationBarHidden(true, animated: true)
+        
+        prepareLabels()
+        prepareButtons()
+    }
+}
+
+// MARK: - Prepares
+
+private extension HomeViewController {
+    
+    func prepareLabels() {
+        resultLabel.text = Constant.ResultLabel.initialText
+        resultLabel.font = Constant.ResultLabel.font
+        resultLabel.textColor = Constant.ResultLabel.textColor
+        resultLabel.textAlignment = .right
+    }
+    
+    func prepareButtons() {
+        numpadButtons.forEach { button in
+            button.titleLabel?.font = Constant.NumpadButtons.titleFont
+            button.setTitleColor(Constant.NumpadButtons.titleColor, for: .normal)
+            button.backgroundColor = Constant.NumpadButtons.backgroundColor
+            button.layer.cornerRadius = Constant.NumpadButtons.cornerRadius
+            button.addTarget(self, action: #selector(numpadButtonTapped(_:)), for: .touchUpInside)
+        }
+
+        basicOperationButtons.forEach { button in
+            button.titleLabel?.font = Constant.BasicOperationButtons.titleFont
+            button.setTitleColor(Constant.BasicOperationButtons.titleColor, for: .normal)
+            button.backgroundColor = Constant.BasicOperationButtons.backgroundColor
+            button.layer.cornerRadius = Constant.BasicOperationButtons.cornerRadius
+            button.addTarget(self, action: #selector(basicOperationsTapped(_:)), for: .touchUpInside)
+        }
+        
+        extraOperationButtons.forEach { button in
+            button.titleLabel?.font = Constant.ExtraOperationButtons.titleFont
+            button.setTitleColor(Constant.ExtraOperationButtons.titleColor, for: .normal)
+            button.backgroundColor = Constant.ExtraOperationButtons.backgroundColor
+            button.layer.cornerRadius = Constant.ExtraOperationButtons.cornerRadius
+            button.addTarget(self, action: #selector(extraOperationsTapped(_:)), for: .touchUpInside)
+        }
+    }
+}
+
+// MARK: - Actions
+
+@objc
+private extension HomeViewController {
+    func numpadButtonTapped(_ sender: UIButton) {
+        presenter.numpadButtonTapped(title: sender.titleLabel?.text)
+    }
+    
+    func basicOperationsTapped(_ sender: UIButton) {
+        presenter.basicOperationsTapped(operation: sender.titleLabel?.text)
+    }
+    
+    func extraOperationsTapped(_ sender: UIButton) {
+        presenter.extraOperationsTapped(title: sender.titleLabel?.text)
+    }
 }
