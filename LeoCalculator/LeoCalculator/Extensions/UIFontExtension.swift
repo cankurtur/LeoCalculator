ff86d589f44f13caab97faabbe486e50b45e9828
cankurtur <cankurtur@gmail.com>
2023-05-03 00:27:19 +0300

    create font extension

---
+++ LeoCalculator/LeoCalculator/Extensions/UIFontExtension.swift
@@ -0,0 +1,17 @@
+//
+//  UIFontExtension.swift
+//  LeoCalculator
+//
+//  Created by Can Kurtur on 1.05.2023.
+//
+
+import UIKit
+
+extension UIFont {
+    
+    /// HelveticaNeue Bold 50
+    static let bold50: UIFont = UIFont(name:"HelveticaNeue-Bold", size: 50.0) ?? .systemFont(ofSize: 50)
+    
+    /// HelveticaNeue Bold 20
+    static let bold20: UIFont = UIFont(name:"HelveticaNeue-Bold", size: 20.0) ?? .systemFont(ofSize: 20)
+}
