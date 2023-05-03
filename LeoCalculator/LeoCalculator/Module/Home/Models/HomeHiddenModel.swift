1fd69e8527238d3caafc3d4492db94c37c87d4fc
cankurtur <cankurtur@gmail.com>
2023-05-04 01:49:20 +0300

    create models

---
+++ LeoCalculator/LeoCalculator/Module/Home/Models/HomeHiddenModel.swift
@@ -0,0 +1,18 @@
+//
+//  HomeHiddenModel.swift
+//  LeoCalculator
+//
+//  Created by Can Kurtur on 4.05.2023.
+//
+
+import Foundation
+
+struct HomeHiddenModel {
+    let addIsHidden: Bool
+    let subtractIsHidden: Bool
+    let multiplyIsHidden: Bool
+    let divideIsHidden: Bool
+    let sinIsHidden: Bool
+    let cosIsHidden: Bool
+    let bitcoinIsHidden: Bool
+}
