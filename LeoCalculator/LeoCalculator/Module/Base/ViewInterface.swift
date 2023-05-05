11e4969cff75baf2fb225e56f48757d0e946f451
cankurtur <cankurtur@gmail.com>
2023-05-05 03:25:52 +0300

    add Hud operations to the view

--- LeoCalculator/LeoCalculator/Module/Base/ViewInterface.swift
+++ LeoCalculator/LeoCalculator/Module/Base/ViewInterface.swift
@@ -5,8 +5,25 @@
 //  Created by Can Kurtur on 1.05.2023.
 //
 
-import Foundation
+import UIKit
 
 public protocol ViewInterface: AnyObject {
+    func showHUD()
+    func showHUD(text: String, onMainThread: Bool)
+    func dismissHUD()
+}
 
+public extension ViewInterface where Self: UIViewController {
+
+    func showHUD() {
+        HUDManager.shared.showHUD(text: Localizable.hudCalculating, onMainThread: false, viewController: self)
+    }
+    
+    func showHUD(text: String, onMainThread: Bool) {
+        HUDManager.shared.showHUD(text: text, onMainThread: onMainThread, viewController: self)
+    }
+    
+    func dismissHUD() {
+        HUDManager.shared.dismissHUD(viewController: self)
+    }
 }
