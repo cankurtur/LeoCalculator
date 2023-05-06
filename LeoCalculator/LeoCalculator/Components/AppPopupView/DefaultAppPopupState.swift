1bf665f6297853af1ade77e26ef4bf966f1cdcb4
cankurtur <cankurtur@gmail.com>
2023-05-06 03:06:49 +0300

    create app popup view

---
+++ LeoCalculator/LeoCalculator/Components/AppPopupView/DefaultAppPopupState.swift
@@ -0,0 +1,38 @@
+//
+//  DefaultAppPopupState.swift
+//  LeoCalculator
+//
+//  Created by Can Kurtur on 6.05.2023.
+//
+
+import Foundation
+
+// MARK: - DefaultAppPopupState
+
+public enum DefaultAppPopupState {
+    case defaultPopup(title: String,
+                      message: String,
+                      buttonTitle: String,
+                      action: (() -> Void)?)
+    case networkErrorPopup(message: String, action: (() -> Void)?)
+    
+    var model: AppPopupViewModel {
+        switch self {
+        case .defaultPopup(let title, let message, let buttonTitle, let action):
+            return AppPopupViewModel(title: title,
+                                     description: message,
+                                     buttonTitle: buttonTitle) {
+                SwiftMessagesManager.shared.hide()
+                action?()
+            }
+            
+        case .networkErrorPopup(let message, let action):
+            return AppPopupViewModel(title: Localizable.appPopupViewError,
+                                     description: message,
+                                     buttonTitle: Localizable.appPopupViewOk) {
+                SwiftMessagesManager.shared.hide()
+                action?()
+            }
+        }
+    }
+}
