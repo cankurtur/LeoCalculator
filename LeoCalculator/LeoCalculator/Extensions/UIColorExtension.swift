7fb1b72c911fad4305112395e8d208558198249e
Can <51170501+cankurtur@users.noreply.github.com>
2023-05-07 23:22:45 +0300

    feature/resources (#20)
    * change app icon
    
    * change app colors
    
    * change popup color

---
+++ LeoCalculator/LeoCalculator/Extensions/UIColorExtension.swift
@@ -0,0 +1,31 @@
+//
+//  UIColorExtension.swift
+//  LeoCalculator
+//
+//  Created by Can Kurtur on 7.05.2023.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    // #595959
+    class func appDarkGray() -> UIColor {
+        return UIColor(named: "AppDarkGray")!
+    }
+    
+    // #7F7F7F
+    class func appLightGray() -> UIColor {
+        return UIColor(named: "AppLightGray")!
+    }
+    
+    // #FF8800
+    class func appOrange() -> UIColor {
+        return UIColor(named: "AppOrange")!
+    }
+    
+    // #D00000
+    class func appRed() -> UIColor {
+        return UIColor(named: "AppRed")!
+    }
+}
